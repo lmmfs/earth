f0673c00d99ad55a45edcaba85cee8a998bc50d7
Sir Panda <lmmfs.1998@gmail.com>
2025-10-16 21:27:49 +0100

    feat: improve log file loxation

--- api/constants.py
+++ api/constants.py
@@ -1,6 +1,6 @@
 MAX_MAGNITUDE = 20
 MIN_MAGNITUDE = 0
 LOG_LEVEL = 'INFO'
-LOG_FILE = 'logs/app.log'
+LOG_FILE = '/app/logs/app.log'
 LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 REFRESH_DATASET_INTERVAL_SECONDS = 10
