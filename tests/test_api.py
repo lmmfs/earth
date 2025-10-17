b06c40e78bc12bf4df36278932cdc63dc5494df3
Luis Fernandes <lmmfs.1998@gmail.com>
2025-10-17 11:19:41 +0100

    feat: Fix test imports, basic tests for get request

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,20 +1,23 @@
 from datetime import datetime
 from zoneinfo import ZoneInfo
 from fastapi.testclient import TestClient
-from api.main import app
+from .context import api
 
-client = TestClient(app)
+client = TestClient(api.main.app)
 
 MOCK_TIME_UTC = datetime(2025, 10, 15, 12, 30, 0, tzinfo=ZoneInfo("UTC"))
 
-MOCK_EARTHQUAKES = [
-    {"id": "A1", "magnitude": 5.0, "time": MOCK_TIME_UTC, "latitude": 123.2, "longitude": 123.2, "depth" : 2.0},
-    {"id": "B2", "magnitude": 6.2, "time": MOCK_TIME_UTC, "latitude": 123.2, "longitude": 123.2, "depth" : 2.0},
-]
+MOCK_EARTHQUAKES = (
+    [
+        {"id": "A1", "magnitude": 5.0, "time": MOCK_TIME_UTC, "latitude": 123.2, "longitude": 123.2, "depth" : 2.0},
+        {"id": "B2", "magnitude": 6.2, "time": MOCK_TIME_UTC, "latitude": 123.2, "longitude": 123.2, "depth" : 2.0},
+    ], 
+    True
+    )
 
-def test_read_earthquakes_success(mocker):
+def test_get_earthquakes_success(mocker):
     mocker.patch(
-        "data_requester.retrieve_recent_earthquakes", 
+        "api.main.retrieve_recent_earthquakes", 
         return_value=MOCK_EARTHQUAKES
     )
     
@@ -23,3 +26,13 @@ def test_read_earthquakes_success(mocker):
     assert response.status_code == 200
     assert len(response.json()) == 2
     assert response.json()[0]["magnitude"] == 5.0
+
+def test_get_earthquakes_not_found(mocker):
+    mocker.patch(
+        "api.main.retrieve_recent_earthquakes", 
+        return_value=(None, False)
+    )
+
+    response = client.get("/earthquakes")
+    assert response.status_code == 404
+
